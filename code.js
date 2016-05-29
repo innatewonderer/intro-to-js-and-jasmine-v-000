5877838ff36d8d749a744091f665c26fa7cbc68f
Jenya Zueva <jzueva@gmail.com>
2016-05-29 19:57:05 -0400

    Done.

--- code.js
+++ code.js
@@ -1,1 +1,17 @@
 'use strict';
+
+function favIceCream(icecreamName){
+  return "I love " + icecreamName;
+}
+
+function shouting(string){
+  return string.toUpperCase();
+}
+
+function theTruth(){
+  return true;
+}
+
+function roundDown(number){
+  return Math.floor(number);
+}
